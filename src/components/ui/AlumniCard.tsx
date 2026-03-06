191331c9fc507b7d2c0750d95f27d5180834bcfa
wonis1 <wjdwo2808@gmail.com>
2026-03-06 16:42:35 +0900

    good

--- src/components/ui/AlumniCard.tsx
+++ src/components/ui/AlumniCard.tsx
@@ -1,16 +1,47 @@
 interface AlumniCardProps {
-  name: string
-  detail: string
+  name: string,
+  photoUrl?: string,
+  school?: {
+    element?: {
+      name: string,
+      graduation: number
+    },
+    middle?: {
+      name: string,
+      graduation: number
+    },
+    high?: {
+      name: string,
+      graduation: number
+    }
+  },
+  region?: string,
+  email?: string,
+  phone?: string
 }
 
-function AlumniCard({ name, detail }: AlumniCardProps) {
+function AlumniCard({ name, photoUrl, school, region, email, phone }: AlumniCardProps) {
   return (
-    <article className="">
-      <div className="">사진</div>
-      <div className="">{name}</div>
-      <p className="">{detail}</p>
+    <article>
+      <div style={{width:'30px'}}>
+        <img src={photoUrl} className="alumni-card__photo"/>
+      </div>
+      <div>{name}</div>
+      <p>{school?.element?.name} {school?.element?.graduation}</p>
+      <p>{school?.middle?.name} {school?.middle?.graduation}</p>
+      <p>{school?.high?.name} {school?.high?.graduation}</p>
+
+      <div>
+        <div> {name} </div>
+        <div> {region} </div>
+        <div> {phone} </div>
+        <div> {email} </div>
+        <div> {school?.element?.name} {school?.element?.graduation} </div>
+        <div> {school?.middle?.name} {school?.middle?.graduation} </div>
+        <div> {school?.high?.name} {school?.high?.graduation} </div>
+      </div>
     </article>
   )
 }
 
-export default AlumniCard
+export default AlumniCard;
--- src/pages/AlumniListPage.tsx
+++ src/pages/AlumniListPage.tsx
@@ -1,12 +1,32 @@
 import AlumniCard from '../components/ui/AlumniCard'
-import SideMenu from '../components/ui/SideMenu'
 import TopBar from '../components/ui/TopBar'
+import SideMenu from '../components/ui/SideMenu'
+import favicon from '../assets/icons/jaewon-favicon.png'
+
 
 const alumni = [
-  { name: '이름', detail: '초등학교 (졸업반)\n중학교 (졸업반)\n고등학교 (졸업반)' },
-  { name: '내 이름', detail: '초등학교\n중학교\n고등학교' },
-  { name: '홍길동', detail: '초등학교\n중학교\n고등학교' },
-  { name: '김동창', detail: '초등학교\n중학교\n고등학교' },
+  {
+    name: '김바보',
+    photoUrl: favicon,
+    school: {
+      element: {
+        name: '서울',
+        graduation: 2010
+      },
+      middle: {
+        name: '일본',
+        graduation: 2013
+      },
+      high: {
+        name: '중국',
+        graduation: 2016
+      }
+    },
+    region: '서울시 관악구',
+    email: 'hyundai@autoever.com',
+    phone: '010-1234-5678'
+
+  }
 ]
 
 function AlumniListPage() {
@@ -15,7 +35,7 @@ function AlumniListPage() {
       <TopBar />
       <div className="">
         <SideMenu items={['초등학교', '중학교', '고등학교', '내 프로필']} />
-
+        
         <main className="">
           <section className="">
             <div className="">
@@ -29,13 +49,23 @@ function AlumniListPage() {
 
           <section className="">
             {alumni.map((person, idx) => (
-              <AlumniCard key={`${person.name}-${idx}`} name={person.name} detail={person.detail} />
+              <AlumniCard key={`${person.name}-${idx}`} 
+              name={person.name}
+              photoUrl={favicon}
+              school={person.school}
+              region={person.region}
+              email={person.email}
+              phone={person.phone}
+              />
             ))}
             <div className="" />
             <div className="" />
             <div className="" />
             <div className="" />
           </section>
+        </main>
+        <main>
+
         </main>
       </div>
     </section>
