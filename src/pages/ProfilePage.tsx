2354287d820d080061d862bcd3bdc7c8ed96eab4
wonis1 <wjdwo2808@gmail.com>
2026-03-06 17:16:53 +0900

    feat: ProfilePage placeholder, file upload 기능 추가

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,6 +1,9 @@
 import SchoolAdd from '../components/ui/SchoolAdd'
 import SideMenu from '../components/ui/SideMenu'
 import TopBar from '../components/ui/TopBar'
+import favicon from '../assets/icons/jaewon-favicon.png'
+
+
 
 function ProfilePage() {
   return (
@@ -10,27 +13,44 @@ function ProfilePage() {
         <SideMenu items={['초등학교', '중학교', '고등학교', '내 프로필']} />
 
         <main className="">
-          <section className="">
-            <h3>내 프로필 수정</h3>
-            <div className="">
+          <form>
+            <section className="">
+              <h3>내 프로필 수정</h3>
               <div className="">
-                <div className="" />
-                <button type="button">사진 변경</button>
+                <div className="">
+                  <div className="" />
+                  <img src={favicon} style={{ width: '100px' }} /> {/* 기존 DB에 등록된 이미지를 가져올 수 있도록 로직 구성 필요 */}
+
+                  <div>
+                    <input type="file" accept='image/*' />{/* 이미지만 받을 수 있게 함 */}
+                  </div> 
+                </div>
+
+                <div className="">
+                  <div>
+                    이름 :
+                    <input type='text' id='name' placeholder='이름을 입력해주세요.'></input>
+                  </div>
+                  <div>
+                    전화번호 :
+                    <input type='number' id='phone' placeholder='전화번호를 입력해주세요.'></input>
+                  </div>
+                  <div>
+                    이메일 :
+                    <input type='email' id='email' placeholder='이메일을 입력해주세요.'></input>
+                  </div>
+                  <div>
+                    지역 :
+                    <input type='text' id='region' placeholder='지역을 입력해주세요.'></input>
+                  </div>
+                </div>
               </div>
 
               <div className="">
-                <div>이름</div>
-                <div>전화번호</div>
-                <div>이메일</div>
-                <div>지역</div>
+                <button type="submit">저장</button>
               </div>
-            </div>
-
-            <div className="">
-              <button type="button">저장</button>
-              <button type="button">취소</button>
-            </div>
-          </section>
+            </section>
+          </form>
 
           <section className="">
             <h3>학교 등록</h3>
@@ -43,8 +63,7 @@ function ProfilePage() {
             </div>
 
             <div className="">
-              <button type="button">저장</button>
-              <button type="button">취소</button>
+              <button type="submit">저장</button>
             </div>
           </section>
 
