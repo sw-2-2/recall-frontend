349e048d5fedc137816eb48daf0e4d718e2d3268
wonis1 <wjdwo2808@gmail.com>
2026-03-09 10:52:30 +0900

    style: 로그인페이지 스타일 수정

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,16 +4,17 @@ import kakaoLoginImg from '../assets/images/kakao_login_large_wide.png'
 
 function LoginPage() {
   return (
-    <section className="">
-      <div className="">
-        <h1>안녕하세요</h1>
-        <h2>RE:CALL 입니다.</h2>
-        <p>찾고 싶은 추억의 친구가 있으신가요?</p>
-
-        <div className="">
-          <span>LOGIN</span>
+    <section>
+      <div className={style.LoginPageBasic}>
+        <div className={style.LoginPageText}>
+          <h1>안녕하세요</h1>
+          <h2>RE:CALL 입니다.</h2>
+          <p>찾고 싶은 추억의 친구가 있으신가요?</p>
+        </div>
+        <div className={style.LoginButton}>
+          <div>LOGIN</div>
           <button type="button">
-            <img src={kakaoLoginImg} alt='카카오 로그인'/>
+            <img src={kakaoLoginImg} />
           </button>
         </div>
       </div>
